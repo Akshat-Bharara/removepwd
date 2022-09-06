9fa498caca8296cb75701cfbf96c7e97e98a7168
Akshat-Bharara <112919654+Akshat-Bharara@users.noreply.github.com>
2022-09-06 19:48:33 +0530

    Add files via upload

---
+++ remove_pwd.py
@@ -0,0 +1,40 @@
+import msoffcrypto
+import io
+import pandas as pd
+import os
+import streamlit as st
+
+st.title('Remove Excel Passwords 🔑')
+
+uploaded_file = st.file_uploader("Choose the excel file",type="xlsx")
+
+pwd = st.text_input('Enter the password: ')
+
+remove = st.button("Remove password")
+
+if uploaded_file is not None and remove:
+    
+    name = uploaded_file.name
+    st.write("Name of file: ",name)
+
+    try:
+        decrypted = io.BytesIO()
+
+        with open(name, "rb") as f:
+            file = msoffcrypto.OfficeFile(f)
+            file.load_key(password=pwd)  # Use password
+            file.decrypt(decrypted)
+
+        df = pd.read_excel(decrypted)
+
+        file_container = st.expander("Check your uploaded .csv")
+        st.write(df)
+
+        os.remove(name)
+
+        df.to_excel(name) 
+
+        st.success("Password has been removed") 
+
+    except:
+        st.warning('Password has already been removed')
